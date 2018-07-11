66e62215330bd1718a474f4795f58f08b7eeb508
qianlicao51 <qianlicaodemo@163.com>
2018-07-11 22:34:17 +0800

    HIVE学习

---
+++ src/main/java/cn/zhuzi/hive/Lower.java
@@ -0,0 +1,18 @@
+package cn.zhuzi.hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * @author 作者 grq 
+ * @version 创建时间：2018年7月11日 下午9:55:16
+ *
+ */
+public class Lower extends UDF {
+	public String evaluate(final String s) {
+		if (s == null) {
+			return null;
+		}
+		return s.toString().toLowerCase();
+	}
+
+}
